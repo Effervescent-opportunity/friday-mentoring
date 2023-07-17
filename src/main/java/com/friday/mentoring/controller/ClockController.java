a56cb7dcaa70d06711bb831b9df444e740281112
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-07-17 15:46:59 +0300

    First task. Fixes
    (cherry picked from commit e122b2f3acc1674922f99a5313893db95d76b4c8)

--- src/main/java/com/friday/mentoring/controller/ClockController.java
+++ src/main/java/com/friday/mentoring/controller/ClockController.java
@@ -1,14 +1,15 @@
 package com.friday.mentoring.controller;
 
 import com.friday.mentoring.service.ClockService;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
 
+/*
+1. produces = MediaType.APPLICATION_JSON_VALUE - лишнее, без него отлично работает;
+ */
 @RestController
 public class ClockController {
 
@@ -21,17 +22,30 @@ public class ClockController {
     /**
      * Получение текущей даты и текущего времени в таймзоне UTC
      */
-    @GetMapping(path = "/time/current/utc", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ZonedDateTime> getCurrentDateTimeInUtc() {
-        return ResponseEntity.ok(clockService.getNowInUtc());
+    @GetMapping(path = "/time/current/utc")
+    public CurrentTimeResponse getCurrentDateTimeInUtc() {
+        var nowInUtc = clockService.getNowInUtc();
+        return new CurrentTimeResponse(nowInUtc);
     }
 
     /**
      * Получение текущей даты и текущего времени в заданной таймзоне (в запросе задаётся форматом IANA `Area/Location`).
      */
-    @GetMapping(path = "/time/current", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ZonedDateTime> getCurrentDateTimeInTimezone(@RequestParam("timezone") String ianaTimezone) {
-        return ResponseEntity.ok(clockService.getNowInTimezone(ianaTimezone));
+    @GetMapping(path = "/time/current")
+    public CurrentTimeResponse getCurrentDateTimeInTimezone(@RequestParam("timezone") String ianaTimezone) {
+        var nowInTimezone = clockService.getNowInTimezone(ianaTimezone);
+        return new CurrentTimeResponse(nowInTimezone);
+    }
+
+    /*
+       2. возврат строки как JSON - ужасная практика (да, технически это валидный JSON, однако для REST API
+          нужно использовать реализацию, совместимую с RFC 4627 JSON-text = object / array, а закидоны RFC 7159/8259
+          JSON-text = ws value ws использовать не нужно по причинам:
+             а) несовместимости с некоторыми клиентами;
+             б) непонимания средним человеком-читателем.
+     */
+    record CurrentTimeResponse(ZonedDateTime timestamp) {
+
     }
 
 }
--- src/test/java/com/friday/mentoring/ApplicationTest.java
+++ src/test/java/com/friday/mentoring/ApplicationTest.java
@@ -1,82 +1,101 @@
 package com.friday.mentoring;
 
+import com.friday.mentoring.controller.ClockController;
 import com.friday.mentoring.service.ClockService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.DateTimeException;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-import static org.apache.commons.lang3.StringUtils.strip;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+/*
+Т.к. сервисный слой уже проверен, здесь можно:
+
+1. использовать меньший объём контекста и поднимать только web часть
+2. проверять, что контроллер отдаёт то, что получает от сервиса - без самодеятельности и модификаций
+
+Разделение тестов на "сервисный" и "web" слой максимально удачно, т.к. на Web слое нельзя тривиальным образом
+проверить те граничные случаи, которые легко проверяются тестом сервиса.
+
+Конкретно этот тест нужно перенести в пакет controller и переименовать в ClockControllerTest.
+
+Заморачиваться с финальностью и областями видимости в тестах - нет смысла.
+Хотя это - дело вкуса и обычно не закрепляется ни в каких правилах.
+ */
+@WebMvcTest(controllers = ClockController.class)
 class ApplicationTest {
-    private static final DateTimeFormatter ISO_FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+    // !!! Очень изящная реализация теста через ISO DateTimeFormatter, она позволяет не заморачиваться
+    // с хаками наподобие isAfter/isBefore для времени, но мысль не была доведена до конца :(
+    DateTimeFormatter ISO_FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
+    @MockBean
+    ClockService clockService;
 
     @Autowired
-    private ClockService clockService;
-
-    @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
     @Test
-    public void getNowInUtcTest() throws Exception {
+    void getNowInUtcTest() throws Exception {
         ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
-
-        MockHttpServletResponse response = mockMvc.perform(get("/time/current/utc"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse();
-
-        assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType());
-
-        ZonedDateTime dateFromService = ZonedDateTime.parse(strip(response.getContentAsString(), "\""), ISO_FORMATTER);
-
-        assertTrue(now.isBefore(dateFromService));
-        assertEquals(now.getOffset(), dateFromService.getOffset());
-        assertTrue(ZonedDateTime.now(ZoneOffset.UTC).isAfter(dateFromService));
+        Mockito.when(clockService.getNowInUtc()).thenReturn(now);
+        mockMvc.perform(get("/time/current/utc")).andExpectAll(
+                status().isOk(),
+                content().contentType("application/json"),
+                jsonPath("timestamp").value(ISO_FORMATTER.format(now))
+        );
     }
 
     @Test
-    public void getNowInTimezoneTest() throws Exception {
+    void getNowInTimezoneTest() throws Exception {
         String ianaTimezone = "Europe/Paris";
         ZoneId zoneId = ZoneId.of(ianaTimezone);
         ZonedDateTime now = ZonedDateTime.now(zoneId);
 
-        MockHttpServletResponse response = mockMvc.perform(get("/time/current?timezone=" + ianaTimezone))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse();
+        Mockito.when(clockService.getNowInTimezone(ianaTimezone)).thenReturn(now);
 
-        assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType());
-
-        ZonedDateTime dateFromService = ZonedDateTime.parse(strip(response.getContentAsString(), "\""), ISO_FORMATTER);
-
-        assertTrue(now.isBefore(dateFromService));
-        assertEquals(now.getOffset(), dateFromService.getOffset());
-        assertTrue(ZonedDateTime.now(ZoneOffset.UTC).isAfter(dateFromService));
+        mockMvc.perform(get("/time/current").param("timezone", ianaTimezone)).andExpectAll(
+                status().isOk(),
+                content().contentType("application/json"),
+                jsonPath("timestamp").value(ISO_FORMATTER.format(now))
+        ).andDo(print());
     }
 
     @Test
-    public void getNowInIncorrectTimezoneTest() throws Exception {
-        mockMvc.perform(get("/time/current?timezone=Asia/Paris"))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
+    void getNowInIncorrectTimezoneTest() throws Exception {
+        var tz = "Asia/Paris";
+        var expectedDetail = "whatever";
+
+        Mockito.when(clockService.getNowInTimezone(tz)).thenThrow(new DateTimeException(expectedDetail));
+
+        mockMvc.perform(get("/time/current").param("timezone", tz))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        // Проверяем интеграционный контракт Problem Details
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value(expectedDetail)
+                ).andDo(print());
+    }
+
+    // Необходим тест реакции системы на пропущенный обязательный параметр timezone,
+    // раз в контроллере он (неявно) объявлен как required
+    @Test
+    void testMissingRequiredParameter() throws Exception {
+        mockMvc.perform(get("/time/current"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json")
+                ).andDo(print());
     }
 
 }
