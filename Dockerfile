9621c1d4bbe7ab151420fed9000a6a84befe3543
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-07-17 16:03:42 +0300

    First task. Final

--- Dockerfile
+++ Dockerfile
@@ -2,26 +2,12 @@
 FROM gradle:8.2.0-jdk17-alpine AS build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-
-# clean нет смысла выполнять, т.к. код а) скопирован б) в свежее окружение
-# сборку удобнее выполнять по build без перечисления
-# см. также
 RUN gradle build --no-daemon
 
 # Package stage
 FROM bellsoft/liberica-openjdk-alpine:17.0.7
 EXPOSE 8080
-
-# 1. благодаря
-# bootJar {
-#  archiveFileName = 'application.jar'
-#}
-# в build.gradle - здесь можно получить предсказуемое имя fat jar и не копировать лишнее.
-# По маске в предыдущей реализации копировались два артефакта:
-# -- friday-mentoring-0.0.1-SNAPSHOT-plain.jar (что неверно);
-# -- friday-mentoring-0.0.1-SNAPSHOT.jar (именно этот jar нужно было копировать).
 COPY --from=build /home/gradle/src/build/libs/application.jar /usr/app/application.jar
-
 # позволяет не запускать процесс от рута (см. также google "container escaping" о том, почему это важно)
 USER 1000:1000
 ENTRYPOINT ["java", "-jar", "/usr/app/application.jar"]
--- src/main/java/com/friday/mentoring/exception/ClockExceptionHandler.java
+++ src/main/java/com/friday/mentoring/exception/ClockExceptionHandler.java
@@ -14,56 +14,9 @@ import java.time.DateTimeException;
 @ControllerAdvice
 public class ClockExceptionHandler extends ResponseEntityExceptionHandler {
 
-/*
-
-Без этой обработки возвращается HTTP 500, например:
-
-➜  ~ http ":8080/time/current?timezone=Europe/Kyiw"
-HTTP/1.1 500
-Connection: close
-Content-Type: application/json
-Date: Fri, 15 Jul 2023 00:00:17 GMT
-Transfer-Encoding: chunked
-
-{
-    "error": "Internal Server Error",
-    "path": "/time/current",
-    "status": 500,
-    "timestamp": "2023-07-15T00:00:17.826+00:00"
-}
-
-Очень правильное решение, что HTTP 500 нам не подходит и нужно действительно использовать код ответа HTTP 400,
-т.к. сервер не испытывает никаких внутренних проблем, это ошибка входных параметров от клиента.
-
-Здесь есть несколько моментов:
-
-1. Чтобы добавить кастомный обработчик, можно использовать новую фичу Spring Boot 3 - Problem Detail
-(https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ProblemDetail.html).
-
-В результате ответ будет таким:
-
-➜  ~ http ":8080/time/current?timezone=Europe/Kyiw"
-HTTP/1.1 400
-Connection: close
-Content-Type: application/problem+json
-Date: Fri, 15 Jul 2023 00:00:30 GMT
-Transfer-Encoding: chunked
-
-{
-    "detail": "Unknown time-zone ID: Europe/Kyiw",
-    "instance": "/time/current",
-    "status": 400,
-    "title": "Bad Request"
-}
-
-
-2. ZoneRulesException обрабатывать не нужно, т.к. он наследуется от DateTimeException и следовательно
-   обрабатывается автоматически.
-
-*/
     @ExceptionHandler(value = DateTimeException.class)
     protected ProblemDetail handleTimezoneParsingErrors(DateTimeException ex, WebRequest request) {
-        var pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
         pd.setDetail(ex.getMessage());
         pd.setType(URI.create(request.getContextPath()));
         return pd;
