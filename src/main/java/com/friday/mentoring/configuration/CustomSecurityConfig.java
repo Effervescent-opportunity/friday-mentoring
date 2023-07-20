067dd7ada63296f5425d70d7a6a0c8fcc7b1dc97
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-07-20 20:35:21 +0300

    Second task

---
+++ src/main/java/com/friday/mentoring/configuration/CustomSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.friday.mentoring.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+/**
+ * Бины для аутентификации и авторизации
+ */
+@Configuration
+@EnableWebSecurity
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .requestMatchers("/auth/login").permitAll()
+                                .requestMatchers("/time/**").hasRole("ADMIN")
+                                .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(authorizeHttpRequests -> authorizeHttpRequests
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.NOT_FOUND)))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails rootUser = User.withUsername("root")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(rootUser);
+    }
+
+}
--- src/test/java/com/friday/mentoring/controller/ClockControllerTest.java
+++ src/test/java/com/friday/mentoring/controller/ClockControllerTest.java
@@ -4,6 +4,7 @@ import com.friday.mentoring.service.ClockService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = ClockController.class)
+@WebMvcTest(controllers = ClockController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class ClockControllerTest {
     DateTimeFormatter ISO_FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
