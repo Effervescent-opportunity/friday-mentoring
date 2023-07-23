a65ea9c84b84902ece575161ff498dde0ade675a
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-07-23 22:00:35 +0300

    third try (todo squash & force push) - 1

--- src/main/java/com/friday/mentoring/service/ClockService.java
+++ src/main/java/com/friday/mentoring/service/ClockService.java
@@ -2,6 +2,8 @@ package com.friday.mentoring.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.time.ZoneId;
@@ -15,10 +17,12 @@ import java.time.ZonedDateTime;
 public class ClockService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ClockService.class);
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')") //todo what's the difference between this and secured?
     public ZonedDateTime getNowInUtc() {
         return ZonedDateTime.now(ZoneOffset.UTC);
     }
 
+    @Secured("ROLE_ADMIN")
     public ZonedDateTime getNowInTimezone(String ianaTimezone) {
         try {
             ZoneId zoneId = ZoneId.of(ianaTimezone);
