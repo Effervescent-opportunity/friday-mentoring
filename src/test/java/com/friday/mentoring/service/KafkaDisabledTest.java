0da1da6910f76118c0a43189344769ea91f4f12b
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-08-19 05:55:16 +0300

    Fourth task. Fixes

---
+++ src/test/java/com/friday/mentoring/service/KafkaDisabledTest.java
@@ -0,0 +1,82 @@
+package com.friday.mentoring.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Тест KafkaProducer при отсутствии доступа к Кафке
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class KafkaDisabledTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Test
+    public void authenticationSuccessTest() throws Exception {
+        mockMvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"user\": \"root\", \"password\": \"password\"}"))
+                .andExpect(status().isOk()).andDo(print());
+
+        Mockito.verify(kafkaTemplate, never()).send(anyString(), any());
+    }
+
+    @Test
+    void authenticationFailureTest() throws Exception {
+        mockMvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"user\": \"noRoot\", \"password\": \"password\"}"))
+                .andExpect(status().isUnauthorized()).andDo(print());
+
+        Mockito.verify(kafkaTemplate, never()).send(anyString(), any());
+    }
+
+    @Test
+    @WithMockUser(value = "root", roles = "ADMIN")
+    void authorizationSuccessTest() throws Exception {
+        mockMvc.perform(get("/time/current/utc")).andExpectAll(
+                status().isOk(),
+                content().contentType("application/json")
+        ).andDo(print());
+
+        Mockito.verify(kafkaTemplate, never()).send(anyString(), any());
+    }
+
+    @Test
+    @WithMockUser(value = "noRoot")
+    void authorizationFailureTest() throws Exception {
+        mockMvc.perform(get("/time/current/utc"))
+                .andExpect(status().isForbidden()).andDo(print());
+
+        Mockito.verify(kafkaTemplate, never()).send(anyString(), any());
+    }
+
+    @Test
+    void authorizationFailureAnonymousUserTest() throws Exception {
+        mockMvc.perform(get("/time/current/utc"))
+                .andExpect(status().isNotFound()).andDo(print());
+
+        Mockito.verify(kafkaTemplate, never()).send(anyString(), any());
+    }
+
+}
