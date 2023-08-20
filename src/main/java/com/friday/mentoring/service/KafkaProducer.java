b96cd686d047ff9848f34646f87b8ddc6e321331
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-08-20 07:32:20 +0300

    fifth try (todo squash & force push) - 1

--- src/main/java/com/friday/mentoring/service/KafkaProducer.java
+++ src/main/java/com/friday/mentoring/service/KafkaProducer.java
@@ -7,8 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -34,12 +36,30 @@ public class KafkaProducer {
         if (kafkaIsActive()) {
             LOGGER.info("Sending message [{}] to Kafka", authEvent);
 
+//            try {
+            CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(authEventsTopic, authEvent);
+
+            SendResult<String, Object> result;
+
             try {
-                this.kafkaTemplate.send(authEventsTopic, authEvent).get(3, TimeUnit.SECONDS);
+                result = future.get(3, TimeUnit.SECONDS);
+                LOGGER.info("Send Result : {}", result);
+                LOGGER.info("Saving entry in db");
                 LOGGER.info("Message was sent");
+//                    messageRepo.save(result.getProducerRecord().key().toString(),result.getProducerRecord().value().toString());
             } catch (Exception ex) {
                 LOGGER.warn("Got exception when sending message to Kafka", ex);
+//                    LOGGER.error("Error publishing message ", e);
+//                    messageRepo.save(result.getProducerRecord().key().toString(),result.getProducerRecord().value().toString());
             }
+
+            //todo thing for timeouts - here I have 3 seconds, there is "max.block.ms" property which could be useful
+            // https://stackoverflow.com/questions/60725906/how-to-verify-sprng-kafka-producer-has-successfully-sent-message-or-not
+            
+//                this.kafkaTemplate.send(authEventsTopic, authEvent).get(3, TimeUnit.SECONDS);
+//            } catch (Exception ex) {
+//                LOGGER.warn("Got exception when sending message to Kafka", ex);
+//            }
         } else {
             LOGGER.info("Auth event [{}] won't be sent to Kafka - Kafka is inactive", authEvent);
         }
