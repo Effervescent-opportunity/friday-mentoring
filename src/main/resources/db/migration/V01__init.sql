d55625112a34a86ec407df518d16c682cdda993b
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-09-02 09:56:25 +0300

    Fifth task. Fixes

---
+++ src/main/resources/db/migration/V01__init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE auth_event
+(
+    id         uuid PRIMARY KEY,
+    ip_address varchar(45) NOT NULL,
+    event_time timestamptz NOT NULL,
+    user_name  varchar(30) NOT NULL,
+    type       varchar(30) NOT NULL
+);
+
+create table outbox
+(
+    id          uuid PRIMARY KEY,
+    created_at  timestamptz NOT NULL,
+    retry_count numeric(3)  NOT NULL,
+    event       jsonb       NOT NULL
+);
+
+comment on table auth_event is 'Table with auth events';
+comment on table outbox is 'Table with messages to send';
