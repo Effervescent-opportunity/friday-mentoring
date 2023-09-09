17674e862ad24ac8a5b3fab118337158864a4d47
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-09-09 20:41:24 +0300

    seventh try (todo squash & force push)

---
+++ src/test/java/com/friday/mentoring/service/AuthEventListenerTest.java
@@ -0,0 +1,7 @@
+package com.friday.mentoring.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthEventListenerTest {//todo write
+
+}
---
+++ src/test/java/com/friday/mentoring/service/AuthEventServiceTest.java
@@ -0,0 +1,7 @@
+package com.friday.mentoring.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthEventServiceTest {//todo write
+
+}
