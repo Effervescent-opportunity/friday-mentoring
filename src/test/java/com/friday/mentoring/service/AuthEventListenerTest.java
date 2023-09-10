96ddec6c9be6eab097b284f78e5ab67778add16b
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-09-10 18:09:11 +0300

    seventh try (todo squash & force push) - 1

--- src/test/java/com/friday/mentoring/service/AuthEventListenerTest.java
+++ src/test/java/com/friday/mentoring/service/AuthEventListenerTest.java
@@ -1,7 +1,72 @@
 package com.friday.mentoring.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.friday.mentoring.dto.AuthEventDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
-class AuthEventListenerTest {//todo write
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AuthEventListenerTest {//todo why spy-mock is resetting between tests? & extract constants
+
+    AuthEventService authEventService = Mockito.spy(Mockito.mock(AuthEventService.class));
+
+    @InjectMocks
+    AuthEventListener authEventListener;
+
+    @Test
+    public void detailsInstanceOfWebAuthenticationDetails() {
+        Map<String, Object> eventData = Map.of("details", new WebAuthenticationDetails("remoteAddress", "sessionId"));
+        AuditApplicationEvent event = new AuditApplicationEvent("user", "type", eventData);
+
+        Mockito.doAnswer(invocation -> {
+            if (invocation.getArgument(0) instanceof AuthEventDto authEventDto) {
+                assertEquals("user", authEventDto.userName());
+                assertEquals("type", authEventDto.type());
+                assertEquals("remoteAddress", authEventDto.ipAddress());
+                assertEquals(OffsetDateTime.ofInstant(event.getAuditEvent().getTimestamp(), ZoneId.systemDefault()), authEventDto.time());
+            } else {
+                Assertions.fail("AuthEventDto expected");
+            }
+            return null;
+        }).when(authEventService).processEvent(any());
+
+        authEventListener.on(event);
+
+        verify(authEventService).processEvent(any(AuthEventDto.class));
+    }
+
+    @Test
+    public void noDetails() {
+        AuditApplicationEvent event = new AuditApplicationEvent("user", "type", "data");
+
+        Mockito.doAnswer(invocation -> {
+            if (invocation.getArgument(0) instanceof AuthEventDto authEventDto) {
+                assertEquals("user", authEventDto.userName());
+                assertEquals("type", authEventDto.type());
+                assertEquals("Unknown", authEventDto.ipAddress());
+                assertEquals(OffsetDateTime.ofInstant(event.getAuditEvent().getTimestamp(), ZoneId.systemDefault()), authEventDto.time());
+            } else {
+                Assertions.fail("AuthEventDto expected");
+            }
+            return null;
+        }).when(authEventService).processEvent(any());
+
+        authEventListener.on(event);
+
+        verify(authEventService).processEvent(any(AuthEventDto.class));
+    }
 
 }
--- src/test/java/com/friday/mentoring/service/AuthEventServiceTest.java
+++ src/test/java/com/friday/mentoring/service/AuthEventServiceTest.java
@@ -1,7 +1,41 @@
 package com.friday.mentoring.service;
 
+import com.friday.mentoring.db.repository.AuthEventRepository;
+import com.friday.mentoring.db.repository.OutboxRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.support.TransactionTemplate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class AuthEventServiceTest {//todo write
+//todo что значит оценить покрытие и реалистичность проверки таких тестов - к каким кейсам устойчивы, к каким - неустойчивы,
+    // что значит тесты неустойчивы к кейсам. входные данные изменятся и тесты не покажут ошибку? хз, мне кажется при норм написанных
+    //тестах они покроют все
+    //юнит тестами не протестировать всякие аспекты, event'ы, контексты, транзакции - сам Спринг, например. или flyway
+    //что при попытке отправить запрос не с тем паролем будет 404 (или не оно, не помню), и создастся event с неуспешной аутентификацией
+    //кажется я не настолько тупая. или это задания легкие?
+
+    @Spy
+    AuthEventRepository authEventRepository;
+    @Spy
+    OutboxRepository outboxRepository;
+    @Mock
+    TransactionTemplate transactionTemplate;
+    @Mock
+    KafkaProducer kafkaProducer;
+
+    @InjectMocks
+    AuthEventService authEventService;
+
+    @Test
+    public void test1() {
+        authEventService.processEvent(null);
+    }
 
 }
