21658e42455ac8209036befcdc1963a4623d50a4
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-09-14 22:04:32 +0300

    seventh try (todo squash & force push) - 6

--- src/testInt/java/com/friday/mentoring/db/repository/AuthEventRepositoryTest.java
+++ src/testInt/java/com/friday/mentoring/db/repository/AuthEventRepositoryTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)//todo why it's working only with this annotation? b
-    //because of this: By default, tests annotated with @DataJpaTest are transactional and roll back at the end of each test. They also use an embedded in-memory database (replacing any explicit or usually auto-configured DataSource). The @AutoConfigureTestDatabase annotation can be used to override these settings.
+        //because of this: By default, tests annotated with @DataJpaTest are transactional and roll back at the end of each test. They also use an embedded in-memory database (replacing any explicit or usually auto-configured DataSource). The @AutoConfigureTestDatabase annotation can be used to override these settings.
 class AuthEventRepositoryTest extends BaseDatabaseIntegrationTest {
 
     @Autowired
@@ -23,25 +23,25 @@ class AuthEventRepositoryTest extends BaseDatabaseIntegrationTest {
 
     @Test
     public void save() {
-        AuthEventDto authEventDto = new AuthEventDto("ipAddress", OffsetDateTime.now(), "user", "type");
+        AuthEventDto eventDto = new AuthEventDto("ip", OffsetDateTime.now(), "user", "type");
 
-        authEventRepository.save(new AuthEventEntity(authEventDto));
+        authEventRepository.save(new AuthEventEntity(eventDto));
 
-        List<AuthEventEntity> authEventEntities = authEventRepository.findAll();
-        assertEquals(1, authEventEntities.size());
-        AuthEventEntity authEventEntity = authEventEntities.get(0);
-        assertNotNull(authEventEntity.getId());
-        assertEquals(authEventDto.ipAddress(), authEventEntity.getIpAddress());
-        assertEquals(authEventDto.time(), authEventEntity.getEventTime());
-        assertEquals(authEventDto.userName(), authEventEntity.getUserName());
-        assertEquals(authEventDto.type(), authEventEntity.getEventType());
+        List<AuthEventEntity> eventEntities = authEventRepository.findAll();
+        assertEquals(1, eventEntities.size());
+        AuthEventEntity eventEntity = eventEntities.get(0);
+        assertNotNull(eventEntity.getId());
+        assertEquals(eventDto.ipAddress(), eventEntity.getIpAddress());
+        assertEquals(eventDto.time(), eventEntity.getEventTime());
+        assertEquals(eventDto.userName(), eventEntity.getUserName());
+        assertEquals(eventDto.type(), eventEntity.getEventType());
 
         authEventRepository.save(new AuthEventEntity(new AuthEventDto("127.0.0.1", OffsetDateTime.now(), "root", "auth")));
 
-        authEventEntities = authEventRepository.findAll();
-        assertEquals(2, authEventEntities.size());
+        eventEntities = authEventRepository.findAll();
+        assertEquals(2, eventEntities.size());
 
-        assertTrue(authEventEntities.contains(authEventEntity));
+        assertTrue(eventEntities.contains(eventEntity));
     }
 
 }
