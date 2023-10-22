72af78a65811bc6e285afaaad1129f4fa7423171
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-10-22 13:54:00 +0300

    tenth try (todo squash & force push) - 3

--- src/main/java/com/friday/mentoring/rest/AuthEventController.java
+++ src/main/java/com/friday/mentoring/rest/AuthEventController.java
@@ -1,16 +1,18 @@
 package com.friday.mentoring.rest;
 
 import com.friday.mentoring.jpa.AuthEventEntity;
+import com.friday.mentoring.usecase.EventRepository;
+import com.friday.mentoring.usecase.SiemEventType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,12 @@ import java.util.List;
 public class AuthEventController {//todo maybe rename to EventController?
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthEventController.class);
+
+    private final EventRepository eventRepository;
+
+    public AuthEventController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
 
     @GetMapping(path = "auth/event", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AuthEventEntity>> getEvents() {
@@ -29,16 +37,24 @@ public class AuthEventController {//todo maybe rename to EventController?
     }
 
     //todo maybe ip by mask? or how it's correct like 127.*.*.*
-    //todo make indexes (word indices is correct by unused)
+    //todo make indexes (word indices is correct but unused)
     //who ?user=root&?ip=127.0.0.5
     //what ?type=Auth_success
     //when ?dateFrom=2015-01-01T16:33:23&?dateTo=2016-03-07T14:55:32
     //page ?page=0?size=100500 (incorrect, max size = 100)
     //sort ?sort=column1,direction1&sort=column2,direction2
-    public void aaaa(      @RequestParam(required = false) String title,
-                           @RequestParam(defaultValue = "0") int page,
-                           @RequestParam(defaultValue = "10") int size,
-                           @RequestParam(defaultValue = "id,desc") String[] sort) {
-        //
+    public void aaaa(@RequestParam(name = "user", required = false) String user,
+                     @RequestParam(name = "ip", required = false) String ip,
+                     @RequestParam(name = "type", required = false) String type,
+                     @RequestParam(name = "dateFrom", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime dateFrom,
+                     @RequestParam(name = "dateTo", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime dateTo,
+                     @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                     @RequestParam(name = "size", required = false, defaultValue = "10") int size,
+                     @RequestParam(name = "sort", required = false, defaultValue = "id,desc") String[] sort) {
+        //https://stackoverflow.com/questions/62676920/spring-boot-offsetdatetime-in-query-param
+        //todo check date parsing, they can not work -> object mapper/ and try spring.mvc.format.date-format=yyyy-MM-dd'T'HH:mm:ss.SSSXXX
+    }
+
+    private record AuthEventDto(String ipAddress, OffsetDateTime time, String userName, SiemEventType eventType) {
     }
 }
--- src/main/java/com/friday/mentoring/usecase/EventRepository.java
+++ src/main/java/com/friday/mentoring/usecase/EventRepository.java
@@ -1,6 +1,7 @@
 package com.friday.mentoring.usecase;
 
 import com.friday.mentoring.jpa.AuthEventEntity;
+import org.springframework.data.domain.Page;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -12,4 +13,6 @@ public interface EventRepository {
     void setSuccessStatus(UUID id);
 
     Stream<AuthEventEntity> getNotSentEvents();
+
+    Page<AuthEventEntity> getFilteredEntities();
 }
