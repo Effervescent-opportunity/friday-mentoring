10379fd590d3a0434d5694e2972d38ddb636f37a
Effervescent Opportunity <41738221+Effervescent-opportunity@users.noreply.github.com>
2023-10-27 10:42:21 +0300

    Tenth task

--- src/main/java/com/friday/mentoring/jpa/AuthEventRepositoryFacade.java
+++ src/main/java/com/friday/mentoring/jpa/AuthEventRepositoryFacade.java
@@ -1,14 +1,40 @@
 package com.friday.mentoring.jpa;
 
 import com.friday.mentoring.usecase.EventRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import static com.friday.mentoring.jpa.AuthEventEntity_.*;
+import static com.friday.mentoring.jpa.AuthEventSpecifications.*;
+
 @Component
 class AuthEventRepositoryFacade implements EventRepository {
+
+    private static final String SORT_DELIMITER = ",";
+    /**
+     * Названия полей, по которым возможна сортировка
+     */
+    private static final List<String> sortedFields;
+
+    static {
+        sortedFields = new ArrayList<>();
+        sortedFields.add(ID);
+        sortedFields.add(USER_NAME);
+        sortedFields.add(IP_ADDRESS);
+        sortedFields.add(EVENT_TYPE);
+        sortedFields.add(EVENT_TIME);
+    }
 
     private final AuthEventRepository authEventRepository;
 
@@ -29,5 +55,65 @@ class AuthEventRepositoryFacade implements EventRepository {
     @Override
     public Stream<AuthEventEntity> getNotSentEvents() {
         return authEventRepository.streamByWasSentFalse();
+    }
+
+    @Override
+    public Page<AuthEventEntity> getFilteredEntities(String userName, String ipAddress, String eventType, OffsetDateTime eventTimeFrom,
+                                                     OffsetDateTime eventTimeTo, int page, int size, String[] sort) {
+        if (page < 0) {
+            throw new IllegalArgumentException("Номер страницы должен быть неотрицательным");
+        }
+
+        if (size <= 0 || size > 100) {
+            throw new IllegalArgumentException("Допустимый размер страницы: от 1 до 100 включительно");
+        }
+
+        if (eventTimeFrom != null && eventTimeTo != null && eventTimeFrom.isAfter(eventTimeTo)) {
+            throw new IllegalArgumentException("Дата начала периода должна быть до даты окончания периода");
+        }
+
+        Specification<AuthEventEntity> spec = userNameEquals(userName).and(ipAddressEquals(ipAddress)).and(eventTypeEquals(eventType))
+                .and(eventTimeGreaterThanOrEquals(eventTimeFrom)).and(eventTimeLessThanOrEquals(eventTimeTo));
+
+        Sort sortObject = getSortFromArray(sort);
+        return authEventRepository.findAll(spec, PageRequest.of(page, size, sortObject));
+    }
+
+    private Sort getSortFromArray(String[] sort) {
+        if (sort.length <= 1) {
+            throw new IllegalArgumentException("Сортировка задана некорректно");
+        }
+
+        List<Order> sortOrders = new ArrayList<>();
+
+        if (sort[0].contains(SORT_DELIMITER)) {
+            // Сортировка по двум и более полям, массив вида: ["userName,desc", "eventType,asc"]
+            for (String sortOrder : sort) {
+                String[] fieldAndDirection = sortOrder.split(SORT_DELIMITER);
+                if (fieldAndDirection.length != 2) {
+                    throw new IllegalArgumentException("Сортировка задана некорректно");
+                }
+                sortOrders.add(getSortOrder(fieldAndDirection[0], fieldAndDirection[1]));
+            }
+        } else {
+            // Сортировка по одному полю, массив вида: ["userName", "desc"]
+            sortOrders.add(getSortOrder(sort[0], sort[1]));
+        }
+
+        return Sort.by(sortOrders);
+    }
+
+    private Order getSortOrder(String fieldName, String direction) {
+        if (!sortedFields.contains(fieldName)) {
+            throw new IllegalArgumentException("Некорректное название поля для сортировки: " + fieldName);
+        }
+
+        if (Direction.ASC.name().equalsIgnoreCase(direction)) {
+            return new Order(Direction.ASC, fieldName);
+        } else if (Direction.DESC.name().equalsIgnoreCase(direction)) {
+            return new Order(Direction.DESC, fieldName);
+        } else {
+            throw new IllegalArgumentException("Некорректное направление сортировки: " + direction);
+        }
     }
 }
---
+++ src/testInt/java/com/friday/mentoring/rest/EventControllerTest.java
@@ -0,0 +1,150 @@
+package com.friday.mentoring.rest;
+
+import com.friday.mentoring.BaseIntegrationTest;
+import com.friday.mentoring.jpa.AuthEventRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DirtiesContext
+@AutoConfigureMockMvc
+public class EventControllerTest extends BaseIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    AuthEventRepository authEventRepository;
+
+    @AfterEach
+    void tearDown() {
+        authEventRepository.deleteAll();
+    }
+
+    @Sql("/add_events.sql")
+    @Test
+    void emptyFilterTest() throws Exception {
+        mockMvc.perform(get("/auth/events"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType("application/json"),
+                        jsonPath("numberOfElements").value(5)
+                ).andDo(print());
+    }
+
+    @Sql("/add_events.sql")
+    @Test
+    void successFiltersTest() throws Exception {
+        mockMvc.perform(get("/auth/events").param("userName", "root")
+                        .param("timeTo", OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+                        .param("ipAddress", "127.0.0.1")
+                        .param("sort", "eventTime,asc")
+                        .param("size", "1"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType("application/json"),
+                        jsonPath("numberOfElements").value(1),
+                        jsonPath("totalPages").value(3),
+                        jsonPath("content[0].eventType").value("AUTHENTICATION_SUCCESS"),
+                        jsonPath("content[0].userName").value("root")
+                ).andDo(print());
+
+        mockMvc.perform(get("/auth/events").param("eventType", "AUTHENTICATION_SUCCESS")
+                        .param("timeFrom", OffsetDateTime.of(2022, 1, 1, 1, 1, 1, 0, ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE_TIME))
+                        .param("timeTo", OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+                        .param("sort", "userName,desc")
+                        .param("sort", "eventTime,asc")
+                        .param("page", "1")
+                        .param("size", "1"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType("application/json"),
+                        jsonPath("numberOfElements").value(1),
+                        jsonPath("totalPages").value(3),
+                        jsonPath("number").value(1),
+                        jsonPath("content[0].eventType").value("AUTHENTICATION_SUCCESS"),
+                        jsonPath("content[0].userName").value("other")
+                ).andDo(print());
+    }
+
+    @Test
+    void incorrectPageNumberTest() throws Exception {
+        mockMvc.perform(get("/auth/events").param("page", "-1"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Номер страницы должен быть неотрицательным")
+                ).andDo(print());
+    }
+
+    @Test
+    void incorrectSizeTest() throws Exception {
+        mockMvc.perform(get("/auth/events").param("size", "0"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Допустимый размер страницы: от 1 до 100 включительно")
+                ).andDo(print());
+
+        mockMvc.perform(get("/auth/events").param("size", "101"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Допустимый размер страницы: от 1 до 100 включительно")
+                ).andDo(print());
+    }
+
+    @Test
+    void incorrectTimeIntervalTest() throws Exception {
+        mockMvc.perform(get("/auth/events").param("timeFrom", OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+                        .param("timeTo", OffsetDateTime.now().minusHours(1).format(DateTimeFormatter.ISO_DATE_TIME)))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Дата начала периода должна быть до даты окончания периода")
+                ).andDo(print());
+    }
+
+    @Test
+    void incorrectSortTest() throws Exception {
+        mockMvc.perform(get("/auth/events").param("sort", "incorrect"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Сортировка задана некорректно")
+                ).andDo(print());
+
+        mockMvc.perform(get("/auth/events").param("sort", "userName,asc")
+                        .param("sort", "incorrect"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Сортировка задана некорректно")
+                ).andDo(print());
+
+        mockMvc.perform(get("/auth/events").param("sort", "user,asc"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Некорректное название поля для сортировки: user")
+                ).andDo(print());
+
+        mockMvc.perform(get("/auth/events").param("sort", "userName,upper"))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith("application/problem+json"),
+                        jsonPath("detail").value("Некорректное направление сортировки: upper")
+                ).andDo(print());
+    }
+}
